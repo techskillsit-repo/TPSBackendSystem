cda3fa64d9ed1650a682534f76e79cd3bd4778c1
madhavi-bari <madhavi-bari>
2021-03-14 16:59:32 +0530

    single executive id added

--- src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
+++ src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
@@ -1,21 +1,26 @@
 package com.tpsbackendsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tpsbackendsystem.model.Executive;
+import com.tpsbackendsystem.repository.ExecutiveRepository;
 
 @RestController
 public class ExecutiveController {
 
+	@Autowired
+	private ExecutiveRepository executiveRepository;
 	
 	@PostMapping("/executive")  
-	public Executive postExecutive(@RequestBody Executive executive){
-		return null;
+	public Executive insertExecitive(@RequestBody Executive executive){
+		return executiveRepository.save(executive);
 	}
 	
 	@GetMapping("/executives") 
@@ -24,7 +29,9 @@ public class ExecutiveController {
 	}
 	
 	@GetMapping("/executive/{id}")
-	public void getSingleExecutive(){
+	public Executive getSingleExecutive(@PathVariable("id") Long id){
+		
+	return executiveRepository.getOne(id);
 		
 	}
 	
