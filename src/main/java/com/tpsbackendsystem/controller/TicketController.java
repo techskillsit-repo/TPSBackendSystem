02926270ce6dbde3fb4e7ecabd15b1487d20e3c5
apnicollege <i.hirani@techskillsit.com>
2021-03-14 12:15:37 +0530

    ticket paging api

--- src/main/java/com/tpsbackendsystem/controller/TicketController.java
+++ src/main/java/com/tpsbackendsystem/controller/TicketController.java
@@ -4,6 +4,9 @@ package com.tpsbackendsystem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,9 +43,12 @@ public class TicketController { //make the controller RestController
 		 
 	}
 	
-	@GetMapping("/tickets")
-	public List<Ticket> getAllTickets(){
-		return ticketRepository.findAll();
+	@GetMapping("/tickets/{page}/{size}")
+	public List<Ticket> getAllTickets(@PathVariable("page") Integer page, 
+			@PathVariable("size") Integer size){
+		
+		Pageable pageable = PageRequest.of(page, size); 
+		return  ticketRepository.findAll(pageable).getContent();
 	}
 	
 	@GetMapping("/ticket/{id}")
