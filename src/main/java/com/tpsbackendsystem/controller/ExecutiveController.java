cc83f636a1a208077ef53728ff7e03065cb1f2fa
apnicollege <i.hirani@techskillsit.com>
2021-03-14 13:33:56 +0530

    executive controller assignment

--- src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
+++ src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
@@ -1,5 +1,43 @@
 package com.tpsbackendsystem.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tpsbackendsystem.model.Executive;
+
+@RestController
 public class ExecutiveController {
 
+	
+	@PostMapping("/executive")  
+	public Executive postExecutive(@RequestBody Executive executive){
+		return null;
+	}
+	
+	@GetMapping("/executives") 
+	public void getAllExecutive(){
+		 
+	}
+	
+	@GetMapping("/executive/{id}")
+	public void getSingleExecutive(){
+		
+	}
+	
+	@PutMapping("/executive/{id}")
+	public void editExecutive(@RequestBody Executive executive){
+		
+	}
+	
+	@DeleteMapping("/executive/{id}")
+	public void deleteExecutive(){
+		
+	}
+	
+	//everyone need to implement this and push the code in your respective branches without fail.
+	
 }
