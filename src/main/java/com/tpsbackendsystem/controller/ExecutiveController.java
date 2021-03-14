8f6c1c42383c07c390be4274bbb48fec0e632836
Rutvij Dave <rutvij@Rutvijs-MacBook-Pro.local>
2021-03-14 21:21:12 +0530

    Executive Controller created

--- src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
+++ src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
@@ -1,12 +1,17 @@
 package com.tpsbackendsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tpsbackendsystem.model.Executive;
@@ -24,25 +29,28 @@ public class ExecutiveController {
 	}
 	
 	@GetMapping("/executives") 
-	public void getAllExecutive(){
-		 
+	public List<Executive> getAllExecutive(@RequestParam(name="page", required=false, defaultValue="0") Integer page,
+										   @RequestParam(name="size", required=false, defaultValue="100") Integer size){
+		Pageable pageable = PageRequest.of(page, size);
+		 return executiveRepository.findAll(pageable).getContent();
 	}
 	
 	@GetMapping("/executive/{id}")
 	public Executive getSingleExecutive(@PathVariable("id") Long id){
-		
-	return executiveRepository.getOne(id);
-		
+		return executiveRepository.getOne(id);		
 	}
 	
 	@PutMapping("/executive/{id}")
-	public void editExecutive(@RequestBody Executive executive){
+	public Executive editExecutive(@PathVariable("id") Long id,@RequestBody Executive executive){
+		Executive executiveDB = executiveRepository.getOne(id);
+		executiveDB.setDepartment(executive.getDepartment());
+		return executiveRepository.save(executiveDB);
 		
 	}
 	
 	@DeleteMapping("/executive/{id}")
-	public void deleteExecutive(){
-		
+	public void deleteExecutive(@PathVariable("id") Long id){
+		executiveRepository.deleteById(id);
 	}
 	
 	//everyone need to implement this and push the code in your respective branches without fail.
