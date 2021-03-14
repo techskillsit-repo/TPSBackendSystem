47de026a3f82b68badfc2f0aa7a45f393737a4e5
Richa <richamandhan65@gmail.com>
2021-03-14 22:55:45 +0530

    Get API for all executives

--- src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
+++ src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
@@ -1,12 +1,17 @@
 package com.tpsbackendsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tpsbackendsystem.model.Executive;
@@ -24,7 +29,12 @@ public class ExecutiveController {
 	}
 	
 	@GetMapping("/executives") 
-	public void getAllExecutive(){
+	public List<Executive> getAllExecutive(
+			@RequestParam(name = "page",required = false, defaultValue = "0") Integer page,
+			@RequestParam(name = "size",required = false,defaultValue = "50") Integer size){
+		
+		Pageable pageable = PageRequest.of(page, size);
+		return executiveRepository.findAll(pageable).getContent();
 		 
 	}
 	
