4992ffc56290ff8710bdee8d6ab66d1f5ec3ab25
MandarGK <mandarkale5@gmail.com>
2021-03-21 10:56:05 +0530

    delete executive added

--- src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
+++ src/main/java/com/tpsbackendsystem/controller/ExecutiveController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tpsbackendsystem.model.Executive;
+import com.tpsbackendsystem.model.Ticket;
 import com.tpsbackendsystem.repository.ExecutiveRepository;
 
 @RestController
@@ -19,7 +21,7 @@ public class ExecutiveController {
 	private ExecutiveRepository executiveRepository;
 	
 	@PostMapping("/executive")  
-	public Executive insertExecitive(@RequestBody Executive executive){
+	public Executive insertExecutive(@RequestBody Executive executive){
 		return executiveRepository.save(executive);
 	}
 	
@@ -36,13 +38,16 @@ public class ExecutiveController {
 	}
 	
 	@PutMapping("/executive/{id}")
-	public void editExecutive(@RequestBody Executive executive){
+	public Executive editExecutive(@PathVariable("id") Long id,@RequestBody Executive executive){
 		
+		Executive executiveDB = executiveRepository.getOne(id);
+		executiveDB.setDepartment(executive.getDepartment());
+		return executiveRepository.save(executiveDB);
 	}
 	
 	@DeleteMapping("/executive/{id}")
-	public void deleteExecutive(){
-		
+	public void deleteExecutive(@RequestParam("id") Long id){
+		executiveRepository.deleteById(id);
 	}
 	
 	//everyone need to implement this and push the code in your respective branches without fail.
